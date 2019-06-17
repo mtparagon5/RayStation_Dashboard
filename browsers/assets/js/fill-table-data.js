01d781e0045fb6cf77a44902a793028628ab5475
Matthew <mt.paragon5@gmail.com>
2019-06-17 18:17:25 -0400

    working on filtering for table and dvh

--- browsers/assets/js/fill-table-data.js
+++ browsers/assets/js/fill-table-data.js
@@ -1,3 +1,43 @@
-function fillDataTables() {
 
+var dataTablesData = [];
+
+
+function fillDataTables(dataTable, data) {
+
+  dataTable.destroy();
+  // fill tables
+  dataTable.DataTable({
+    data: data,
+    columns: [
+      // structure
+      // Structure: data[i][0],
+      // PlanID: data[i][1],
+      // MaxDose: data[i][2],
+      // "Delta Max": data[i][3],
+      // D95: data[i][4],
+      // "Delta D95": data[i][5],
+
+      // <th>Structure</th>
+      // <th>Plan</th>
+      // <th>Max Dose (Gy)</th>
+      // <th>Max %&#916;</th>
+      // <th>D95 (Gy)</th>
+      // <th>D95 %&#916;</th>
+
+      { title: "Structure" },
+      { title: "Plan" },
+      { title: "Max Dose [Gy]" },
+      { title: "Max %&#916" },
+      { title: "D95 [Gy]" },
+      { title: "D95 %&#916" }
+    ],
+    paging: true,
+    fixedHeader: {
+      header: true,
+      // footer: true
+    },
+    autoWidth: false,
+    pageLength: 50,
+
+  });
 };
